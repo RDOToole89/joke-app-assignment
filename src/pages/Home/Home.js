76d20cb74a659cc8301579b6b69fdba5dc6a31fc
RDOToole89 <roibinotoole@gmail.com>
2020-12-15 23:09:26 +0100

    Add text-bubble with Styled-Components

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,41 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import './Home.css';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const TextBaloon = styled.div`
+  & {
+    -webkit-filter: drop-shadow(-1px -1px 2px rgba(0, 0, 0, 0.1))
+      drop-shadow(1px 2px 2px rgba(0, 0, 0, 0.15));
+    filter: drop-shadow(-1px -1px 2px rgba(0, 0, 0, 0.1))
+      drop-shadow(1px 2px 2px rgba(0, 0, 0, 0.15));
+    margin: 1rem;
+    margin-bottom: 40px;
+    padding: 1.5rem 2rem;
+    position: relative;
+    font-family: 'Source Sans Pro', sans-serif;
+    font-size: 1.2rem;
+    font-weight: 400;
+    background: #8ca8d8;
+    color: white;
+  }
+  &:before {
+    border: 12.5px solid transparent;
+    border-top: 12.5px solid #8ca8d8;
+    border-bottom: 0;
+    height: 0;
+    width: 0;
+    border-top-width: 25px;
+    content: '';
+    display: block;
+    position: absolute;
+    left: 3rem;
+    bottom: -25px;
+    -webkit-transform-origin: center;
+    transform-origin: center;
+    -webkit-transform: rotate(90deg) skew(-25deg) translateY(16.6666666667px);
+    transform: rotate(90deg) skew(-25deg) translateY(16.6666666667px);
+  }
+`;
+
+const Cite = styled.div`
+  position: absolute;
+  bottom: -2rem;
+  left: 4.5rem;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 300;
+  letter-spacing: 0.5px;
+  color: black;
+`;
 
 function Home() {
   const [joke, setJoke] = useState('');
-
-  const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: palevioletred;
-  `;
-
-  const TextBaloon = styled.p`
-    & {
-      position: relative;
-      max-width: 30em;
-
-      background-color: #fff;
-      padding: 1.125em 1.5em;
-      font-size: 1.25em;
-      border-radius: 1rem;
-      box-shadow: 0 0.125rem 0.5rem rgba(0, 0, 0, 0.3), 0 0.0625rem 0.125rem rgba(0, 0, 0, 0.2);
-    }
-    &:before {
-      content: '';
-      position: absolute;
-      width: 0;
-      height: 0;
-      bottom: 100%;
-      left: 1.5em; // offset should move with padding of parent
-      border: 0.75rem solid transparent;
-      border-top: none;
-
-      // looks
-      border-bottom-color: #fff;
-      filter: drop-shadow(0 -0.0625rem 0.0625rem rgba(0, 0, 0, 0.1));
-    }
-  `;
 
   console.log('JOKE IS', joke);
 
@@ -64,8 +84,14 @@ function Home() {
 
   return (
     <div className='Home'>
-      <Title>Hilarious Programmer Jokes!</Title>
-      <TextBaloon>Hello World!</TextBaloon>
+      <div>
+        <Title>Hilarious Programmer Jokes!</Title>
+
+        <TextBaloon>
+          <p>{joke && joke[0].setup}</p>
+          <Cite>Nerd</Cite>
+        </TextBaloon>
+      </div>
     </div>
   );
 }
