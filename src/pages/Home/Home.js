5b4d05d6105776277562255ed0bebbe95024cc89
RDOToole89 <roibinotoole@gmail.com>
2020-12-15 22:34:15 +0100

    Setup joke useState

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Home() {
+  const [joke, setJoke] = useState('');
+
+  console.log('JOKE IS', joke);
+
   useEffect(() => {
     const fetchJoke = async () => {
       try {
         const response = await fetch(
           `https://official-joke-api.appspot.com/jokes/programming/random`
         );
+
+        if (!response.ok) {
+          throw new Error(`Bad request`);
+        }
+
         const joke = await response.json();
+
+        if (joke) {
+          setJoke(joke);
+        }
       } catch (e) {
         console.log(e);
       }
