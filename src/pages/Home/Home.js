34f078c1e2a9648612cfe4e13d4c1acfb3765a03
RDOToole89 <roibinotoole@gmail.com>
2020-12-15 22:47:32 +0100

    Add text balloon with styled components

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,41 @@
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
 function Home() {
   const [joke, setJoke] = useState('');
+
+  const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+  `;
+
+  const TextBaloon = styled.p`
+    & {
+      position: relative;
+      max-width: 30em;
+
+      background-color: #fff;
+      padding: 1.125em 1.5em;
+      font-size: 1.25em;
+      border-radius: 1rem;
+      box-shadow: 0 0.125rem 0.5rem rgba(0, 0, 0, 0.3), 0 0.0625rem 0.125rem rgba(0, 0, 0, 0.2);
+    }
+    &:before {
+      content: '';
+      position: absolute;
+      width: 0;
+      height: 0;
+      bottom: 100%;
+      left: 1.5em; // offset should move with padding of parent
+      border: 0.75rem solid transparent;
+      border-top: none;
+
+      // looks
+      border-bottom-color: #fff;
+      filter: drop-shadow(0 -0.0625rem 0.0625rem rgba(0, 0, 0, 0.1));
+    }
+  `;
 
   console.log('JOKE IS', joke);
 
@@ -30,7 +64,8 @@ function Home() {
 
   return (
     <div className='Home'>
-      <h1>Joke App!</h1>
+      <Title>Hilarious Programmer Jokes!</Title>
+      <TextBaloon>Hello World!</TextBaloon>
     </div>
   );
 }
